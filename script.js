6550988ff7b0bbca435c32ff3b9f63cbf5d60a96
Theo Bouteiller <theo.bouteiller@etu.univ-amu.fr>
2019-02-13 21:57:19 +0100

    Creation de plusieur fonction :  isCanMoveRock(x, y, direction)  elle renvoie true ou false celon si la pierre peux etre bouger
     isCleanCell(x,y)
     renvoie true ou false celon si la case peux etre une case dite 
    marchable
    
     Implementation du pousage des pierre

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ class Tableau {
       let tabLigne = [];
 
       for (let col of ligne) {
+
         if (col == " "){
           col = espaceInsecable;
         }
@@ -58,6 +59,8 @@ $( document ).ready(function () {
     });;
   }
 
+  //deplacement
+
   $('body').keydown(function(event) {
     inputKey = event['key'];
 
@@ -81,43 +84,186 @@ $( document ).ready(function () {
   });
 });
 
+//logique
+
 function goLeft() {
-  if(tabMapLoad[posY][posX - 1]["ct"] == espaceInsecable){
-    changeCell(posX, posY, espaceInsecable);
-    posX -= 1;
-    changeCell(posX, posY, "@");
+  switch (tabMapLoad[posY][posX - 1]["ct"]) {
+    case espaceInsecable:
+      changeCell(posX, posY, espaceInsecable);
+      posX -= 1;
+      changeCell(posX, posY, "@");
+      break;
+
+    case "$":
+      let posPierreX = posX - 1;
+      let posPierreY = posY;
+      //verifier si la pierre peux bouger donc une case plus loin
+      if (isCanMoveRock(posPierreX, posPierreY, "left")){
+        //deplacer la pierre remplacer sa case par le joueur
+        changeCell(posPierreX, posPierreY, "@")
+        changeCell(posPierreX - 1, posPierreY, "$")
+        //remettre a jour la case initiale du joueur
+        changeCell(posX, posY, espaceInsecable);
+        posX -= 1;
+      }
+      break;
+
+    case ".":
+      //changeCell(posX, posY, espaceInsecable);
+
+      //posX -= 1;
+      //changeCell(posX, posY, "@");
+      break;
+    default:
+
   }
 }
 
 function goRight() {
-  if(tabMapLoad[posY][posX + 1]["ct"] == espaceInsecable){
-    changeCell(posX, posY, espaceInsecable);
-    posX += 1;
-    changeCell(posX, posY, "@");
+  switch (tabMapLoad[posY][posX + 1]["ct"]) {
+    case espaceInsecable:
+      changeCell(posX, posY, espaceInsecable);
+      posX += 1;
+      changeCell(posX, posY, "@");
+      break;
+
+    case "$":
+      let posPierreX = posX + 1;
+      let posPierreY = posY;
+      //verifier si la pierre peux bouger donc une case plus loin
+      if (isCanMoveRock(posPierreX, posPierreY, "right")){
+        //deplacer la pierre remplacer sa case par le joueur
+        changeCell(posPierreX, posPierreY, "@")
+        changeCell(posPierreX + 1, posPierreY, "$")
+        //remettre a jour la case initiale du joueur
+        changeCell(posX, posY, espaceInsecable);
+        posX += 1;
+      }
+      break;
+
+    case ".":
+      //changeCell(posX, posY, espaceInsecable);
+
+      //posX -= 1;
+      //changeCell(posX, posY, "@");
+      break;
+    default:
+
   }
 }
 
 function goUp() {
-  if(tabMapLoad[posY - 1][posX]["ct"] == espaceInsecable){
-    changeCell(posX, posY, espaceInsecable);
-    posY -= 1;
-    changeCell(posX, posY, "@");
+  switch (tabMapLoad[posY - 1][posX]["ct"]) {
+    case espaceInsecable:
+      changeCell(posX, posY, espaceInsecable);
+      posY -= 1;
+      changeCell(posX, posY, "@");
+      break;
+
+    case "$":
+      let posPierreX = posX;
+      let posPierreY = posY - 1;
+      //verifier si la pierre peux bouger donc une case plus loin
+      if (isCanMoveRock(posPierreX, posPierreY, "up")){
+        //deplacer la pierre remplacer sa case par le joueur
+        changeCell(posPierreX, posPierreY, "@")
+        changeCell(posPierreX, posPierreY - 1, "$")
+        //remettre a jour la case initiale du joueur
+        changeCell(posX, posY, espaceInsecable);
+        posY -= 1;
+      }
+      break;
+
+    case ".":
+      //changeCell(posX, posY, espaceInsecable);
+
+      //posX -= 1;
+      //changeCell(posX, posY, "@");
+      break;
+    default:
+
   }
 }
 
 function goDown() {
-  if(tabMapLoad[posY + 1][posX]["ct"] == espaceInsecable){
-    changeCell(posX, posY, espaceInsecable);
-    posY += 1;
-    changeCell(posX, posY, "@");
+  switch (tabMapLoad[posY + 1][posX]["ct"]) {
+    case espaceInsecable:
+      changeCell(posX, posY, espaceInsecable);
+      posY += 1;
+      changeCell(posX, posY, "@");
+      break;
+
+    case "$":
+      let posPierreX = posX;
+      let posPierreY = posY + 1;
+      //verifier si la pierre peux bouger donc une case plus loin
+      if (isCanMoveRock(posPierreX, posPierreY, "down")){
+        //deplacer la pierre remplacer sa case par le joueur
+        changeCell(posPierreX, posPierreY, "@")
+        changeCell(posPierreX, posPierreY + 1, "$")
+        //remettre a jour la case initiale du joueur
+        changeCell(posX, posY, espaceInsecable);
+        posY += 1;
+      }
+      break;
+
+    case ".":
+      //changeCell(posX, posY, espaceInsecable);
+
+      //posX -= 1;
+      //changeCell(posX, posY, "@");
+      break;
+    default:
+
   }
 }
 
-function afficherPos() {
-  console.log("posX : " + posX + " posY : " + posY);
-}
+//utilitaire pour logique
 
 function changeCell(posX, posY, char) {
   tabMapLoad[posY][posX]["ct"] = char;
   tabMapLoad[posY][posX]["obj"].empty().append(char);
+}
+
+function isCanMoveRock(x, y, direction) {
+  switch (direction) {
+    case "up":
+      return isCleanCell(x, y - 1);
+      break;
+
+    case "down":
+      return isCleanCell(x, y + 1);
+      break;
+
+    case "left":
+      return isCleanCell(x - 1, y);
+      break;
+
+    case "right":
+      return isCleanCell(x + 1, y);
+      break;
+    default:
+      return false;
+  }
+}
+
+function isCleanCell(x, y) {
+  switch (tabMapLoad[y][x]["ct"]) {
+    case "#":
+      return false;
+      break;
+
+    case "$":
+      return false;
+      break;
+
+    default:
+      return true;
+  }
+}
+
+// debug
+
+function afficherPos() {
+  console.log("posX : " + posX + " posY : " + posY);
 }
